1f8f45def2da3a2ae259c9155fc0fdf9083d1331
Martin Sydor <martin.sydor@disqo.com>
2022-02-27 19:40:45 -0600

    Configure font-end tests;

--- reservation-app/src/app/reservation.service.spec.ts
+++ reservation-app/src/app/reservation.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReservationService } from './reservation.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('ReservationService', () => {
   let service: ReservationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports : [
+        HttpClientModule
+      ]
+    });
     service = TestBed.inject(ReservationService);
   });
 
